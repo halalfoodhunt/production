56f7229acc1a05ee6ca32368862d2704eefc1898
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-20 13:51:50 +0000

    added price range on models

--- app/models/price_range.rb
+++ app/models/price_range.rb
@@ -1,3 +1,10 @@
 class PriceRange < ActiveRecord::Base
     has_many :places
+    has_many :ecommers
+    has_many :food_deliveries
+    has_many :caterers
+    has_many :supermarkets
+    has_many :online_grocers
+    has_many :lessons
+    has_many :suppliers
 end
