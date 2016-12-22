4ef4554409952053bfa8df269bad7bbaae0f42f3
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-22 09:33:11 +0000

    edit

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,22 +4,32 @@ class Plan < ActiveRecord::Base
   include Koudoku::Plan
   
   Plan.create({
+      name: 'FREE',
+      price: 00.00,
+      interval: 'month',
+      stripe_id: 'free',
+      features: ['1 Unverified Listing', '1 Featured Image', '1 Category', '1 Highlight', '1 Location', '1 Featured Question'].join("\n\n"),
+      display_order: 1
+    })
+  
+  
+  Plan.create({
       name: 'Basic',
       price: 18.00,
+      highlight: true, # This highlights the plan on the pricing page.
       interval: 'month',
       stripe_id: 'basic',
-      features: ['2 Listings', '1 Page', '1 User', '1 Organization'].join("\n\n"),
-      display_order: 1
+      features: ['2 Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward'].join("\n\n"),
+      display_order: 2
     })
 
     Plan.create({
       name: 'Basic Plus',
-      highlight: true, # This highlights the plan on the pricing page.
       price: 28.00,
       interval: 'month',
       stripe_id: 'basic-plus',
-      features: ['5 Listings', '3 Pages', '3 Users', '3 Organizations'].join("\n\n"),
-      display_order: 2
+      features: ['5 Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward', 'Email Function', 'Review/Feedback Add-on'].join("\n\n"),
+      display_order: 3
     })
 
     Plan.create({
@@ -27,8 +37,8 @@ class Plan < ActiveRecord::Base
       price: 68.00, 
       interval: 'month',
       stripe_id: 'social', 
-      features: ['Unlimited Listings', '10 Pages', '10 Users', '10 Organizations'].join("\n\n"), 
-      display_order: 3
+      features: ['Unlimited Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward', 'Email Function', 'Review/Feedback Add-on'].join("\n\n"), 
+      display_order: 4
     })
   
 end
