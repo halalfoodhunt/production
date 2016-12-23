1d77d0c50a073b157c10d7876bbe52d9c0df7929
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-23 00:28:01 +0000

    edited

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,41 +3,4 @@ class Plan < ActiveRecord::Base
 
   include Koudoku::Plan
   
-  Plan.create({
-      name: 'FREE',
-      interval: 'month',
-      stripe_id: 'free',
-      features: ['1 Unverified Listing', '1 Featured Image', '1 Category', '1 Highlight', '1 Location', '1 Featured Question'].join("\n\n"),
-      display_order: 1
-    })
-  
-  
-  Plan.create({
-      name: 'Basic',
-      price: 18.00,
-      highlight: true, # This highlights the plan on the pricing page.
-      interval: 'month',
-      stripe_id: 'basic',
-      features: ['2 Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward'].join("\n\n"),
-      display_order: 2
-    })
-
-    Plan.create({
-      name: 'Basic Plus',
-      price: 28.00,
-      interval: 'month',
-      stripe_id: 'basic-plus',
-      features: ['5 Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward', 'Email Function', 'Review/Feedback Add-on'].join("\n\n"),
-      display_order: 3
-    })
-
-    Plan.create({
-      name: 'Social',
-      price: 68.00, 
-      interval: 'month',
-      stripe_id: 'social', 
-      features: ['Unlimited Verified Listings', 'Unlimited Categories', 'Unlimited Highlights', 'Unlimited Locations', '5 Featured Questions', 'Meta Word Search', 'Reservation Listing', 'Delivery Listing', 'FRIENDS Reward', 'Email Function', 'Review/Feedback Add-on'].join("\n\n"), 
-      display_order: 4
-    })
-  
 end
