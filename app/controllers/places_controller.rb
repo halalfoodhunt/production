8b37f76483e3403c6cab3d2b622af7be1b23d45c
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-28 10:17:11 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -28,6 +28,8 @@ class PlacesController < ApplicationController
   # GET /places/1
   # GET /places/1.json
   def show
+    @place = Place.find(params[:id])
+    @day = @place.days.build
   end
 
   # GET /places/new
