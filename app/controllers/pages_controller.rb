88cfefe57433bdfad36fb778a8b54fd949d11854
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-29 07:26:24 +0000

    edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -54,7 +54,6 @@ class PagesController < ApplicationController
   def friends_rewards
     @friends_rewards = FriendsReward.all
     @places = Place.all
-    @rewards_categories = RewardsCategory.all
   end
 
 end
