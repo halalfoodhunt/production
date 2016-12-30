7290717649465213b70dcee4741441fb0e410d14
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-30 05:43:40 +0000

    edited

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,7 @@ class Place < ActiveRecord::Base
   accepts_nested_attributes_for  :opening_hours
   
   before_create :set_expiration_date
+  before_create :set_halal_expiry
 
   validates_presence_of :merchant_id
   validates_presence_of :business_name
@@ -31,6 +32,10 @@ class Place < ActiveRecord::Base
   
   def set_expiration_date
     self.expiry_date =  Date.today + 365.days
+  end
+  
+  def halal_expiry
+    self.halal_expiry =  Date.today + 365.days
   end
   
   has_attached_file :logo, styles: { large: "300x300>", medium: "120x120>", thumb: "30x30>" }, default_url: "/images/:style/placeholder.png"
