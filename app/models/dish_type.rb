6c4c00e37c4a03445ff999f51932ae77a321e483
Allyssa Deanna <dee@halalfoodhunt.com>
2016-12-30 10:42:49 +0000

    edited

--- app/models/dish_type.rb
+++ app/models/dish_type.rb
@@ -1,4 +1,5 @@
 class DishType < ActiveRecord::Base
     belongs_to :food_type
 	belongs_to :ecommer
+	belongs_to :supplier
 end
--- app/models/food_type.rb
+++ app/models/food_type.rb
@@ -1,6 +1,7 @@
 class FoodType < ActiveRecord::Base
     has_many :dish_types
 	has_many :ecommers, :through => :dish_types
+	has_many :suppliers, :through => :dish_types
 	
 	has_attached_file :icon, styles: { large: "300x300>", medium: "120x120>", square: "30x30>", thumb: "16x16>" }, default_url: "/images/:style/logo-placeholder.png"
     validates_attachment_content_type :icon, content_type: /\Aimage\/.*\Z/
