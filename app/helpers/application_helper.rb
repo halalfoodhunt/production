de398f51a19f3d186eaadb4b180f508dff9578df
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-03 09:11:09 +0000

    edited

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-    def resource_name
-    :merchant
-  end
-
-  def resource
-    @resource ||= Merchant.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:merchant]
-  end
 end
