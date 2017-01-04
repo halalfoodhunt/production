4093e8e12c9d41730bc8f21a7deecbec0dcd7804
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-04 06:19:38 +0000

    edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -39,10 +39,11 @@ class PagesController < ApplicationController
  def lessons
  end
  
- def places
-  @places = Place.all
-  @friends_rewards = FriendsReward.all
-end
+    def places
+    @search = Place.ransack(params[:q])
+    @places = @search.result
+    @friends_rewards = FriendsReward.all
+    end
 
 def online_grocers
 end
