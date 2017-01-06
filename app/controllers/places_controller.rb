8c7e07361fd41b12031254d1307cdca6c15b22d5
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-06 08:00:40 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -75,7 +75,7 @@ class PlacesController < ApplicationController
     @place.cuisine_type_ids = params[:place][:cuisine_type_ids]
     @place.highlight_ids = params[:place][:highlight_ids]
     @place.dining_type_ids = params[:place][:dining_type_ids]
-    7.times { @place.opening_hours.build }
+
   end
 
   # DELETE /places/1
