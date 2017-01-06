24e76c56e00ef8558e7c73b0a970b40e681b7112
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-06 10:10:39 +0000

    edited

--- app/controllers/merchants/registrations_controller.rb
+++ app/controllers/merchants/registrations_controller.rb
@@ -11,7 +11,7 @@ class Merchants::RegistrationsController < Devise::RegistrationsController
 	private
 	
 	def after_sign_up_path_for(resource)
-    koudoku.pricing_path
+    koudoku_pricing_path
 	end
 
 	
