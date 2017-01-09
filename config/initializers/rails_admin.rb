eeffbad0f381e6167f3dc2de1b3dbcb0e5db4691
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-09 04:57:26 +0000

    edited

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,7 @@ RailsAdmin.config do |config|
   config.current_user_method(&:current_merchant)
     
   config.authorize_with do
-    redirect_to main_app.root_path unless current_merchant.is_admin?
+    redirect_to main_app.root_path unless current_merchant.admin?
   end
 
   ## == Cancan ==
