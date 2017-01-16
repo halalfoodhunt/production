6ec115cc2dacc18e13455dd6dec69f530f1f8591
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-16 07:14:42 +0000

    edited

--- app/controllers/food_deliveries_controller.rb
+++ app/controllers/food_deliveries_controller.rb
@@ -57,7 +57,7 @@ class FoodDeliveriesController < ApplicationController
       end
     end
     @food_delivery.food_delivery_type_ids = params[:food_delivery][:food_delivery_type_ids]
-    @food_delivery.cuisine_type_ids = params[:food_delivery][:cuisine_ids]
+    @food_delivery.cuisine_type_ids = params[:food_delivery][:cuisine_type_ids]
   end
 
   # DELETE /food_deliveries/1
