ef81e2444e41bc34680d39f6190e6def8152ebba
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-26 05:50:07 +0000

    edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,8 @@ class PagesController < ApplicationController
    @users_testimonials = UsersTestimonial.all
    @featured_articles = FeaturedArticle.all
    if params[:special_tags].blank? 
-    @places = Place.all.where(draft: false)
+   @search = Place.ransack(params[:q])
+   @places = @search.result.where(draft: false)
    else
     @special_tag_ids = SpecialTag.find_by(name: params[:special_tags]).id
     @places = Place.all.where(special_tag_ids: @special_tag_ids).where(draft: false)
