a6035676075a7ab9b8bcc55f2268cf404b7d796e
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-28 02:22:53 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,9 @@ class PlacesController < ApplicationController
     if params[:special_tag].present? 
     @special_tag_id = SpecialTag.find_by(name: params[:special_tag]).id
     @places = Place.joins(:admin_tags).where(admin_tags: {special_tag_id: @special_tag_id})
+    @search = Place.ransack(params[:q])
+    @search.sorts = 'created_at DESC' if @search.sorts.empty?
+    @places = @search.result.where(draft: false)
     else
     @search = Place.ransack(params[:q])
     @search.sorts = 'created_at DESC' if @search.sorts.empty?
