5ac340499282a0958025c456865e3e63f1ba6737
Allyssa Deanna <dee@halalfoodhunt.com>
2017-01-28 01:54:36 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,9 +5,14 @@ class PlacesController < ApplicationController
   # GET /places
   # GET /places.json
   def index
+    if params[:special_tag].blank? 
     @search = Place.ransack(params[:q])
     @search.sorts = 'created_at DESC' if @search.sorts.empty?
     @places = @search.result.where(draft: false)
+    else
+    @special_tag_id = SpecialTag.find_by(name: params[:special_tag]).id
+    @places = Place.joins(:admin_tags).where(admin_tags: {special_tag_id: @special_tag_id})
+    end
     @qualifying_type = QualifyingType.all
     @hash = Gmaps4rails.build_markers(@places) do |place, marker|
     marker.lat place.latitude
