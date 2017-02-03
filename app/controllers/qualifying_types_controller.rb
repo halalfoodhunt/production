d9909e64b7a9b8fbdb95d9e63baec973c36a22b8
Allyssa Deanna <dee@halalfoodhunt.com>
2017-02-03 09:47:23 +0000

    edited

--- app/controllers/qualifying_types_controller.rb
+++ app/controllers/qualifying_types_controller.rb
@@ -76,6 +76,6 @@ class QualifyingTypesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def qualifying_type_params
-      params.require(:qualifying_type).permit(:name, :icon)
+      params.require(:qualifying_type).permit(:name, :icon, :halal_summary)
     end
 end
