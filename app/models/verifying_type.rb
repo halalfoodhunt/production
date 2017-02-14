4c218a342c33664c2b23fecadb1753e9230fc77d
Allyssa Deanna <dee@halalfoodhunt.com>
2017-02-14 08:16:39 +0000

    edited

--- app/models/verifying_type.rb
+++ app/models/verifying_type.rb
@@ -8,6 +8,6 @@ class VerifyingType < ActiveRecord::Base
     has_many :suppliers
     has_many :food_deliveries
     
-    has_attached_file :icon, styles: { large: "300x300>", medium: "120x120>", square: "30x30>", thumb: "16x16>" }, default_url: "/images/:style/logo-placeholder.png"
+    has_attached_file :icon, styles: { large: "300x300>", medium: "120x120>", square: "40x40>", thumb: "16x16>" }, default_url: "/images/:style/logo-placeholder.png"
     validates_attachment_content_type :icon, content_type: /\Aimage\/.*\Z/
 end
