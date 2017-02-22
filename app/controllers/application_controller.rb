1f0e18da3f30ad63b0276e45af8ae9ddca590cc9
Allyssa Deanna <dee@halalfoodhunt.com>
2017-02-22 04:12:47 +0000

    edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_path, :alert => exception.message
   end
+  
+  def after_sign_in_path_for(resource)
+  places_path
+  end
 end
