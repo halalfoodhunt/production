8a599329b26d3b3dd8a71a25d700dbd7dbf32b94
Allyssa Deanna <dee@halalfoodhunt.com>
2017-03-02 08:57:18 +0000

    edited

--- app/controllers/merchants/registrations_controller.rb
+++ app/controllers/merchants/registrations_controller.rb
@@ -5,7 +5,7 @@ class Merchants::RegistrationsController < Devise::RegistrationsController
 	end
 	
 	def account_update_params
-		params.require(:merchant).permit(:registered_business_name, :email, :password, :current_password, :uen_roc_fhc, :contact_person_name, :contact_person_position, :office_number, :mobile_number, :office_mailing_address, :logo, :region_id, :role)
+		params.require(:merchant).permit(:registered_business_name, :email, :password, :uen_roc_fhc, :contact_person_name, :contact_person_position, :office_number, :mobile_number, :office_mailing_address, :logo, :region_id, :role)
 	end
 	
 	private
