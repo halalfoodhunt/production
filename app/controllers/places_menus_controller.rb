b89e183951ba86c4a4ebf09c3ca13cdd03bfac2b
Allyssa Deanna <dee@halalfoodhunt.com>
2017-03-06 04:20:20 +0000

    edited

--- app/controllers/places_menus_controller.rb
+++ app/controllers/places_menus_controller.rb
@@ -28,7 +28,7 @@ class PlacesMenusController < ApplicationController
 
     respond_to do |format|
       if @places_menu.save
-        format.html { redirect_to @places_menu, notice: 'Places menu was successfully created.' }
+        format.html { redirect_to places_menus_path, notice: 'Places menu was successfully created.' }
         format.json { render :show, status: :created, location: @places_menu }
       else
         format.html { render :new }
