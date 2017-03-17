9bd7ee4df3d43466583b4675bc6d49f85fee083c
Allyssa Deanna <dee@halalfoodhunt.com>
2017-03-17 06:08:44 +0000

    edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -49,6 +49,7 @@ class PlacesController < ApplicationController
   # GET /places/new
   def new
     @current_merchant = current_merchant
+    @listings = Listing.all
     @place = Place.new(listing_id: params[:listing_id])
     7.times { @place.opening_hours.build }
   end
