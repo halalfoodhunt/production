8c5cc8e88627e82b5e0eb919dfa1a5210cc17b05
Allyssa Deanna <dee@halalfoodhunt.com>
2017-03-21 21:10:54 +0000

    edited

--- app/controllers/food_deliveries_controller.rb
+++ app/controllers/food_deliveries_controller.rb
@@ -14,6 +14,13 @@ class FoodDeliveriesController < ApplicationController
   # GET /food_deliveries/1
   # GET /food_deliveries/1.json
   def show
+    @food_delivery = FoodDelivery.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js # show.js.erb
+      format.json { render json: @food_delivery }
+    end
   end
 
   # GET /food_deliveries/new
@@ -93,6 +100,6 @@ class FoodDeliveriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def food_delivery_params
-      params.require(:food_delivery).permit(:brand_name, :general_email, :contactable_hours, :general_contact_number, :website, :ordering_link, :facebook, :instagram, :question_1, :question_2, :question_3, :question_4, :question_5, :expiry_date, :halal_expiry, :friends_rewards_terms, :merchant_id, :draft, :verified, :friends_reward_id, :listing_id, :region_id, :cuisine_type_id, :food_delivery_type_id, :price_range_id, :friends_code, :qualifying_type_id, :featured_image, :image, :image_2, :image_3, :image_4, :document_1, :document_2, :document_3, :document_4, :logo, food_item_ids: [])
+      params.require(:food_delivery).permit(:brand_name, :general_email, :contactable_hours, :general_contact_number, :website, :ordering_link, :facefood_delivery, :instagram, :question_1, :question_2, :question_3, :question_4, :question_5, :expiry_date, :halal_expiry, :friends_rewards_terms, :merchant_id, :draft, :verified, :friends_reward_id, :listing_id, :region_id, :cuisine_type_id, :food_delivery_type_id, :price_range_id, :friends_code, :qualifying_type_id, :featured_image, :image, :image_2, :image_3, :image_4, :document_1, :document_2, :document_3, :document_4, :logo, food_item_ids: [])
     end
 end
