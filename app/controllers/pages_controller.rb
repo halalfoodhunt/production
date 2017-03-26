147dd762ea584b290f5a1b39a0933588b51da53c
Allyssa Deanna <dee@halalfoodhunt.com>
2017-03-26 12:05:53 +0000

    edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -190,7 +190,8 @@ def online_grocers_friends_rewards
     @search = OnlineGrocer.ransack(params[:q])
     @online_grocers = @search.result.where(friends_reward_id: @friends_reward_id).order("created_at DESC").where(draft: false)
   if params[:friends_reward].blank?
-    @online_grocers = OnlineGrocer.all.order("created_at DESC").where(draft: false)
+    @search = OnlineGrocer.ransack(params[:q])
+    @online_grocers = @search.result.where(draft: false).where.not(friends_reward_id: nil)
   else
     @friends_reward_id = FriendsReward.find_by(name: params[:friends_reward]).id
     @online_grocers = OnlineGrocer.where(friends_reward_id: @friends_reward_id).order("created_at DESC").where(draft: false)
