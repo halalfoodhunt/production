03bbe1b49b105adea837c9e55c2817849e1295bf
Allyssa Deanna <dee@halalfoodhunt.com>
2017-04-10 00:52:16 +0000

    edited

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ActiveRecord::Base
   extend FriendlyId
   friendly_id :business_name, use: :slugged
   
+  acts_as_commontator
   acts_as_commontable
   
   before_create :set_expiration_date
