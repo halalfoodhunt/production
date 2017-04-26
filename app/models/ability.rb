4f916182c988d291c4ab4abbde595210ca9fea60
Allyssa Deanna <dee@halalfoodhunt.com>
2017-04-26 04:57:49 +0000

    edited

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,37 +18,59 @@ class Ability
     can :create, Place
     
     can :read, Ecommer do |ecommer|
-        Ecommer.merchant == merchant
+        ecommer.merchant == merchant
     end
     can :update, Ecommer do |ecommer|
-        Ecommer.merchant == merchant
+        ecommer.merchant == merchant
     end
     can :destroy, Ecommer do |ecommer|
-        Ecommer.merchant == merchant
+        ecommer.merchant == merchant
     end
     can :create, Ecommer
     
     can :read, Supplier do |supplier|
-        Supplier.merchant == merchant
+        supplier.merchant == merchant
     end
     can :update, Supplier do |supplier|
-        Supplier.merchant == merchant
+        supplier.merchant == merchant
     end
     can :destroy, Supplier do |supplier|
-        Supplier.merchant == merchant
+        supplier.merchant == merchant
     end
     can :create, Supplier
     
     can :read, Caterer do |caterer|
-        Caterer.merchant == merchant
+        caterer.merchant == merchant
     end
     can :update, Caterer do |caterer|
-        Caterer.merchant == merchant
+        caterer.merchant == merchant
     end
     can :destroy, Caterer do |caterer|
-        Caterer.merchant == merchant
+        caterer.merchant == merchant
     end
     can :create, Caterer
+    
+    can :read, Lesson do |lesson|
+        lesson.merchant == merchant
+    end
+    can :update, Lesson do |lesson|
+        lesson.merchant == merchant
+    end
+    can :destroy, Lesson do |lesson|
+        lesson.merchant == merchant
+    end
+    can :create, Lesson
+    
+    can :read, FoodDelivery do |food_delivery|
+        lesson.merchant == merchant
+    end
+    can :update, FoodDelivery do |food_delivery|
+        lesson.merchant == merchant
+    end
+    can :destroy, FoodDelivery do |food_delivery|
+        lesson.merchant == merchant
+    end
+    can :create, FoodDelivery
     end
   end
 end
