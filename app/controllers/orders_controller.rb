006fcc422616a228fbc2933ca14798a6012cf284
Allyssa Deanna <dee@halalfoodhunt.com>
2017-05-12 07:25:19 +0000

    edited

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,30 @@
 class OrdersController < ApplicationController
-  def create
-  end
-
-  def new
+ def new
+  	@product = Product.find(params[:product_id])
+    @order = Order.new
+  	params[:friends_cards].to_i.times { @order.friends_cards.build }
   end
 
   def show
+    @order = Order.find(params[:id])
+    @product = @order.product
   end
+
+  def create
+  	@order = Order.new(order_params)
+  	if @order.save
+  		redirect_to order_path(@order)
+      flash[:success] = "Product has been successfully ordered."
+  	else
+     @product = Product.find(params[:product_id])
+  		render 'new'
+  	end
+
+  end
+
+  private
+
+    def order_params
+    	params.require(:order).permit(:product_id, friends_cards_attributes: [:id, :name, :email, :date_of_birth])
+    end
 end
