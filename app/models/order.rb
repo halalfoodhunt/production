4827f26a15222fb6f4ffc90a259877ce8f0f226b
Allyssa Deanna <dee@halalfoodhunt.com>
2017-05-12 07:52:21 +0000

    edited

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ActiveRecord::Base
   belongs_to :product
   belongs_to :friend
   has_many :order_items
-  has_many :friends_card, :through => :order_items
+  has_many :friends_cards, :through => :order_items
   
   validates :product_id, presence: true
 	accepts_nested_attributes_for :friends_cards
