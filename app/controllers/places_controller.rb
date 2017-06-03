85b1a26f988b759e51818c42a3f33ce5a2cb3e49
Allyssa Deanna <dee@halalfoodhunt.com>
2017-06-03 01:50:44 +0000

    added sitemap

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -47,7 +47,7 @@ class PlacesController < ApplicationController
     @canonical_url = places_path(@place)
     @og_properties = {
       title: @meta_title,
-      type:  'website',
+      type:  'article',
       image: @place.image.url(:thumb),  # this file should exist in /app/assets/images/logo.png
       url: @canonical_url
     }
