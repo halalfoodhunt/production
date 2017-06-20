07c6c02001b7c252b039feb43603faaad3357564
Allyssa Deanna <dee@halalfoodhunt.com>
2017-06-20 04:38:40 +0000

    edited

--- app/controllers/merchants/registrations_controller.rb
+++ app/controllers/merchants/registrations_controller.rb
@@ -1,7 +1,7 @@
 
 class Merchants::RegistrationsController < Devise::RegistrationsController
   before_action :load_plans, only: [:new]
-  after_action :load_plans, only: [:new]
+  after_create :load_plans, only: [:new]
   
   def load_plans
       @plans = ::Plan.order(:display_order)
