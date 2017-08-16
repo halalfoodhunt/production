edce8d850ac2d0c876697319b6f5bd5a9b485f8f
Allyssa Deanna <dee@halalfoodhunt.com>
2017-08-16 16:14:26 +0000

    added friends card

--- app/controllers/friends_cards_controller.rb
+++ app/controllers/friends_cards_controller.rb
@@ -1,6 +1,18 @@
 class FriendsCardsController < ApplicationController
-  before_action :set_friends_reward, only: [:show, :edit, :update, :destroy]
+  before_action :set_friends_card, only: [:show, :edit, :update, :destroy]
+  
   def index
     @friends_cards = FriendsCard.all
   end
+  
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friends_card
+      @friends_card = FriendsCard.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def friends_card_params
+      params.require(:friends_card).permit(:name, :price, :friend_id, :recurring, :period, :cycles)
+    end
 end
