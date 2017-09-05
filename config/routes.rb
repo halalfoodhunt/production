7f089b235514ad1094945c2f1660257e4b8c34bf
Allyssa Deanna <dee@halalfoodhunt.com>
2017-09-05 05:00:40 +0000

    edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :feedbacks
   post '/rate' => 'rater#create', :as => 'rate'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Added by Koudoku.
