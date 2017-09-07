3983633afcd134db4edd6d47fd569c61a5c493b2
Allyssa Deanna <dee@halalfoodhunt.com>
2017-09-07 04:31:16 +0000

    edited

--- app/controllers/friends/registrations_controller.rb
+++ app/controllers/friends/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Friends::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
+force_ssl
 
   def new
     @friends_cards = FriendsCard.all
