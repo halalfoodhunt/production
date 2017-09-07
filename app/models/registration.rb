8f0bd9dce5e1c527d0dc879a99af38989b622943
Allyssa Deanna <dee@halalfoodhunt.com>
2017-09-07 12:01:34 +0000

    edited

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -9,8 +9,7 @@ class Registration < ActiveRecord::Base
   before_create :set_expiration_date
   after_create :send_friends_card_purchase_email
     
-  def send_friends_card_purchase_email (friend, registration)
-    @friend = friend
+  def send_friends_card_purchase_email (registration)
     @registration = registration
     FriendCardNotifier.new_friends_card_purchase_notification(self).deliver
   end
