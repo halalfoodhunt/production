5bd4265d88ea0787a3d3b058ecd5733279d800d6
Allyssa Deanna <dee@halalfoodhunt.com>
2017-09-07 09:37:23 +0000

    edited

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -7,7 +7,6 @@ class Registration < ActiveRecord::Base
   validates :name, :date_of_birth,  :contact_number, :instagram_account, :address_1, :zipcode, presence: true
   
   before_create :set_expiration_date
-  after_create :new_friends_card_purchase_notification
   
   def set_expiration_date
     self.expiry_date =  Date.today + 365.days
