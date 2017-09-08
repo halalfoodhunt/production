61713301b69b672d0b0d3a4e314a51aa27ae7875
Allyssa Deanna <dee@halalfoodhunt.com>
2017-09-08 06:27:57 +0000

    edited

--- app/mailers/friends_card_purchase_notifier.rb
+++ app/mailers/friends_card_purchase_notifier.rb
@@ -1,5 +1,5 @@
 class FriendsCardPurchaseNotifier < ApplicationMailer
-     default from: "dee@halalfoodhunt.com"
+     default from: "info@halalfoodhunt.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,6 +8,6 @@ class FriendsCardPurchaseNotifier < ApplicationMailer
   #
   def new_friends_card_subscription_notification (registration)
     @registration = registration
-    mail to: "dee@halalfoodhunt.com", subject: "New Friends Card Subscription"
+    mail to: "info@halalfoodhunt.com", subject: "New Friends Card Subscription"
   end
 end
