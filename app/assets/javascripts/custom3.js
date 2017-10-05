cbc1c958e35fdf99b39f280d2a2c8bec68283508
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-05 02:44:09 +0000

    edited

--- app/assets/javascripts/custom3.js
+++ app/assets/javascripts/custom3.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($){
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "40%";
-    document.getElementById("push-content").style.marginLeft = "40%";
+    document.getElementById("push-content").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
