fa4222943eaf31c4fc56f79d4ce9bf144741e18c
Allyssa Deanna <dee@halalfoodhunt.com>
2017-10-05 02:34:07 +0000

    edited

--- app/assets/javascripts/custom3.js
+++ app/assets/javascripts/custom3.js
@@ -9,3 +9,15 @@ jQuery(document).ready(function($){
         });
 
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("push-content").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("push-content").style.marginLeft = "0";
+}
